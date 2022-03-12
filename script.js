6f59530c04892c982cf61c874f1db1e356ab4126
rmassey95 <rmassey.95@gmail.com>
2022-03-12 17:18:13 -0500

    Create functionality to continue operations

--- script.js
+++ script.js
@@ -31,15 +31,18 @@ let displayValue = '';
 
 btns.forEach(btn =>{
   btn.addEventListener('click', (e) => {
-    if (e.target.id == '='){
-      displayValue = operate(displayValue);
-    } else if ((e.target.id == '+') || (e.target.id == '*') || (e.target.id == '-') || (e.target.id == '/')) {
-      displayValue += ` ${e.target.id} `;
+    if ((e.target.id == '+') || (e.target.id == '*') || (e.target.id == '-') || (e.target.id == '/')) {
+      displayValue += ` ${e.target.id}`;
+    } else if (displayValue.includes(' ')) {
+      displayValue += ` ${e.target.id}`;
     } else {
       displayValue += e.target.id;
     }
     if (e.target.id == 'clear'){
       displayValue = '';
+    }
+    if ((e.target.id == '=') || (displayValue.split(' ').length == 3)){
+      displayValue = operate(displayValue);
     }
     const outputContainer = document.querySelector('#output');
     outputContainer.textContent = displayValue;
