234b8b42f912c7142e8e8312f7d5b436578ff14b
rmassey95 <rmassey.95@gmail.com>
2022-03-12 15:31:49 -0500

    Create operate function to run certain operation

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 function add(a,b){
   return a + b;
 }
-
 function subtract(a,b){
   return a - b;
 }
-
 function multiply(a,b){
   return a * b;
 }
-
 function divide(a,b){
   return a / b;
 }
 
-console.log(add(2,2));
-console.log(subtract(2,2));
-console.log(multiply(2,2));
-console.log(divide(2,2));
+function operate(operator, num1, num2){
+  switch(operator){
+    case '+':
+      return add(num1,num2);
+      break;
+    case '-':
+      return subtract(num1,num2);
+      break;
+    case '*':
+      return multiply(num1,num2);
+      break;
+    case '/':
+      return divide(num1,num2);
+      break;
+  }
+}
+
+console.log(operate('+',2,2));
+console.log(operate('-',2,2));
+console.log(operate('*',2,2));
+console.log(operate('/',2,2));
